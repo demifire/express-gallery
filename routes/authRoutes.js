06053f5a845d0825a5ebe9bf8dad5666edf5727d
Harsh Kotak <kotak_harsh@hotmail.com>
2018-10-03 18:06:50 -1000

    🐛 stil broken

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,52 +1,144 @@
 const express = require('express');
 const Router = express.Router();
+
 const Users = require('../knex/models/Users.js')
-// const passport = require('passport');
-// const LocalStrategy = require('passport-local');
-// const bcrypt = require('bcrypt');
+const Passport = require('passport');
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
 
 
-
-Router.post('/register', (req, res) => {
-  // const { email, username, password } = req.body;
-  const info = req.body;
-  newInfo = info;
-  const postObj = {
-    email: newInfo.email,
-    username: newInfo.username,
-    password: newInfo.password
-  }
-  console.log('req.body: ', postObj);
-  Users
-    .forge(postObj)
-    .save()
-    .then(result => {
-      if (result) {
-        res.send('new user made');
-      } else {
-        res.send('error making user');
-      }
-    })
-    .catch(err => {
-      res.json(err);
-    })
-});
-
-Router.post('/login', (req, res) => {
-
-});
-
-Router.post('/logout', (req, res) => {
-
-});
-
-Router.get('/protected', (req, res) => {
-  res.send('protected')
+Passport.serializeUser((user, done) => {
+  done(null, user.id);
 })
 
+Passport.deserializeUser((user, done) => {
+  Users
+    .where({ id: user.id })
+    .fetch()
+    .then(user => {
+      user = user.toJSON();
+      done(null, user)
+    })
+    .catch(err => {
+      return done(err)
+    })
+})
 
+// CONFIGURATION TO VERIFY CALLBACK FOR LOCAL AUTHENTICATION
+Passport.use(new LocalStrategy({ usernameField: 'username' }, (username, password, done) => {
+  Users
+    .where({ username })
+    .fetch()
+    .then(user => {
+      // user = user.toJSON();
+      console.log('fuckkkkk: ', user);
 
+      bcrypt.compare(password, user.attributes.password)
+        .then(result => {
+          console.log('RESSSS: ', result);
+          console.log('pass: ', password);
+          console.log('user.att: ', user.attributes.password);
+          if (result) {
+            console.log('asdf ', user);
+            done(null, user)
+          } else {
+            done(null, false)
+          }
+        })
+        .catch(err => {
+          done(null, false)
+        })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}));
 
+// Passport.use(new LocalStrategy(function (username, password, done) {
+//   return new Users({ username: username })
+//     .fetch()
+//     .then(user => {
+//       if (user === null) {
+//         return done(null, false, { message: 'bad username or password' });
+//       } else {
+//         user = user.toJSON();
+//         bcrypt.compare(password, user.password)
+//           .then(samePassword => {
+//             if (samePassword) { return done(null, user); }
+//             else {
+//               return done(null, false, { message: 'bad username or password' });
+//             }
+//           })
+//       }
+//     })
+//     .catch(err => {
+//       return done(err);
+//     });
+// }));
+
+//USE BCRYPT TO SALT THE PASSWORDS SO THEY ARE NOT VISABLE
+const SALT_ROUND = 10
+
+//RENDERS THE USER REGISTRATION FORM
+Router.get('/auth/register', (req, res) => {
+  res.render('./auth/register');
+});
+
+//POSTS THE USER REGISTRATION DATA TO DB AND REDIRECTS TO GALLERY
+Router.post('/auth/register', (req, res) => {
+  const { email, username, password } = req.body;
+
+  bcrypt.hash(password, 10)
+    .then(salt => {
+      return bcrypt.hash(password, salt)
+    })
+    .then(hash => {
+      return Users
+        .forge({ email, username, password: hash })
+        .save()
+    })
+    .then(user => {
+      user = user.toJSON()
+      console.log('user: ', user);
+      // res.json(user) //What exactly does this do??
+      // res.sendStatus(200) //What 
+      res.redirect('/')
+    })
+});
+
+Router.get('/auth/login', (req, res) => {
+  res.render('./auth/login');
+});
+
+Router.post('/auth/login', Passport.authenticate('local', {
+  successRedirect: '/',
+  // failureRedirect: '/auth/login',
+  // failureFlash: true
+}), (req, res) => {
+})
+
+// Router.post('/auth/login', (req, res, next) => {
+//   console.log('login get', req.body)
+//   // req.body.username = req.body.username.toLowerCase();
+//   Passport.authenticate('local', (err, user, info) => {
+//     console.log('user: ', user)
+//     if (err) {
+//       req.flash('error', `wrong username or password`);
+//       return res.redirect('/auth/login')
+//     } else if (!user) {
+//       req.flash('error', `wrong username or password`);
+//       return res.redirect('/auth/login')
+//     } else if (req.body.username < 1 || req.body.password.length < 1) {
+//       req.flash('error', `wrong username or password`);
+//       return res.redirect('/auth/login')
+//     }
+//     req.login(user, (err) => {
+//       if (err) { return next(err); }
+//       return res.redirect('/');
+//       // return res.json({ success: true })
+//     });
+//   })(req, res, next);
+// });
 
 
 
--- server.js
+++ server.js
@@ -4,13 +4,20 @@ const bp = require('body-parser');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const PORT = process.env.EXPRESS_CONTAINER_PORT;
+// const flash = require('connect-flash');
+
 
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 
+const passport = require('passport');
 
 const Gallery = require('./routes/gallery.js');
 const AuthRoutes = require('./routes/authRoutes.js');
+
+//RUN MIDDLEWARE
+app.use(express.static('public'));
+app.use(bp.urlencoded({ extended: true }));
 
 app.use(session({
   store: new RedisStore({ url: 'redis://redis-session-store:6379', logErrors: true }),
@@ -19,15 +26,17 @@ app.use(session({
   saveUninitialized: true
 }))
 
-
-
-//RUN MIDDLEWARE
-app.use(express.static('public'));
-app.use(bp.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// app.use(flash())
+
 app.set('view engine', '.hbs');
+
 
 //ROUTER
 app.use('/', Gallery);
