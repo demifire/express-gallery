ae2df8f13c8738a730f0af4d6f2297afdfd82765
Chaz Kondo <Chaz@Macbook-Pro-2.local>
2018-10-04 19:07:20 -1000

    Fixed login issue

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -113,7 +113,7 @@ Router.get('/login', (req, res) => {
   res.render('./auth/login');
 });
 
-Router.post('/login', Passport.authenticate('local', { failureRedirect: '/login' }), (req, res) => {
+Router.post('/login', Passport.authenticate('local', { failureRedirect: '/auth/login' }), (req, res) => {
   res.redirect('/gallery');
 })
 
@@ -128,10 +128,11 @@ Router.get('/secret', isAuthenticated, (req, res) => {
 })
 
 function isAuthenticated(req, res, next) {
-  // will pass on to the next middleware if successfully authenticated
-  if (req.isAuthenticated()) { next(); }
-  // need to redirect to error page or notify user somehow that permission has been denied
-  else { res.redirect('/'); }
+  if ( req.isAuthenticated() ) { 
+    next(); 
+  } else { 
+    res.redirect('/');
+  }
 };
 
 // function isAuthenticated(req, res, done) {
